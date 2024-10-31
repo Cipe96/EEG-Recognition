b7925ae9de5fcc154de4fe794aa997558ea677fa
Marco Cipollina <marco.cipo8@gmail.com>
2024-10-31 20:55:10 +0100

    Update shared_utilities.py

--- shared_utilities.py
+++ shared_utilities.py
@@ -1,5 +1,6 @@
 # IMPORT LIBRERIE
 import gdown
+import glob
 import os
 # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
 
@@ -17,5 +18,17 @@ def download_dataset(file_id, output, msg=False):
   gdown.download(url, output, quiet=False)
   if msg:
     print(f"\nFile scaricato e salvato come {output}!\n")
+
+# -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
+
+def list_edf_files(base_folder, run_number):
+    # Costruisci il pattern del file in base al numero di run fornito
+    file_pattern = f"S*R01.edf"  # In questo dataset abbiamo solo la run 1
+    # Crea il percorso completo per il pattern
+    full_pattern = os.path.join(base_folder, file_pattern)
+    # Usa glob per trovare tutti i file che corrispondono al pattern
+    file_list = glob.glob(full_pattern)
+    # Restituisce la lista dei percorsi dei file
+    return file_list
 
 # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
