eeb6a28fdbbdc5470703aa1cfec0eab8123d9db1
Marco Cipollina <marco.cipo8@gmail.com>
2024-11-14 17:00:13 +0100

    Update shared_utilities.py
    Aggiornato con le uniche cose facilmente condividibili fra notebook

--- shared_utilities.py
+++ shared_utilities.py
@@ -1,6 +1,6 @@
 # IMPORT LIBRERIE
+import tensorflow as tf
 import gdown
-import glob
 import os
 # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
 
@@ -21,14 +21,41 @@ def download_dataset(file_id, output, msg=False):
 
 # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
 
-def list_edf_files(base_folder, run_number):
-    # Costruisci il pattern del file in base al numero di run fornito
-    file_pattern = f"S*R01.edf"  # In questo dataset abbiamo solo la run 1
-    # Crea il percorso completo per il pattern
-    full_pattern = os.path.join(base_folder, file_pattern)
-    # Usa glob per trovare tutti i file che corrispondono al pattern
-    file_list = glob.glob(full_pattern)
-    # Restituisce la lista dei percorsi dei file
-    return file_list
+def convolutivo(classi, shape, filtri, dropout, strati):
+  '''
+  Funzione per scaricare il dataset.
+  
+  Parametri:
+  - shape: Input shape per il modello.
+  - filtri: numero di filtri iniziali che si raddoppiano ad ogni strato.
+  - dropout: si dimezza ad ogni strato ed è la percentuale di dati che dimentica per migliorare la generalizzazione.
+  - strati: numero di strati convolutivi prima di passare al Dense
+  '''
+  
+  # Aggiungi un layer di input all'inizio per specificare la forma
+  inputs = tf.keras.layers.Input(shape)
+
+  # Utilizziamo strati convoluzionali per estrarre caratteristiche locali
+  for i in range(strati):
+    if i == 0:
+      x = tf.keras.layers.Conv1D(filters=filtri, kernel_size=3, padding='same', activation='relu')(inputs)
+    else:
+      x = tf.keras.layers.SeparableConv1D(filters=filtri, kernel_size=3, padding='same', activation='relu')(inputs)
+
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(dropout)(x)
+
+    filtri *= 2
+    dropout /= 2
+
+  #Appiattiamo tutto primo di consegnare al Dense
+  x = tf.keras.layers.GlobalAveragePooling1D()(x)
+
+  # Aggiungiamo uno strato Dense per la classificazione finale
+  outputs = tf.keras.layers.Dense(classi, activation='softmax')(x)
+
+  model = tf.keras.Model(inputs=inputs, outputs=outputs)
+  
+  return model
 
 # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # -- -- # 
